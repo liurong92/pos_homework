cbb7cf6255a879809e6460f2c8e10472c94c5d66
liurong <570899307@qq.com>
2015-02-04 12:01:39 -0500

    test pass two cartitems for scanner.js

--- __tests__/scanner_tests.js
+++ __tests__/scanner_tests.js
@@ -3,12 +3,39 @@ jest.dontMock('lodash');
 
 describe('Scanner', function () {
   describe('#addCartItems', function () {
-    it('should return one cartItem', function () {
-      var Scanner = require('../src/model/scanner.js');
-      var scanner = new Scanner();
+    var Scanner, scanner;
+    beforeEach(function () {
+      Scanner = require('../src/model/scanner.js');
+      scanner = new Scanner();
+    });
 
+    it('should return one cartItem is brand', function () {
       var result = scanner.addCartItems([{ 'ITEM000000' : 20 }]);
       expect(result[0].item.brand).toEqual('可口可乐');
     });
+
+    it('should return two cartItem', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+
+      var cartItems = [{'item' :
+                              {'barcode' :'ITEM000000',
+                               'name' : '可口可乐350ml',
+                               'unit' : '瓶',
+                               'price' : 3.00,
+                               'brand' : '可口可乐'},
+                        'count' : 20},
+
+                        {'item' :
+                              {'barcode' :'ITEM000010',
+                               'name' : '可口可乐550ml',
+                               'unit' : '瓶',
+                               'price' : 4.00,
+                               'brand' : '可口可乐'},
+                        'count' : 20}
+                        ];
+      expect(result).toEqual(cartItems);
+    });
+
   });
 });
