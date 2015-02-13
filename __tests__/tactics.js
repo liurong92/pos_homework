c0828c713e6a79ac9f094ed32831bd10a3679ae3
liurong <570899307@qq.com>
2015-02-12 20:44:45 -0500

    test getCommonCartItems_in_Tactics_class

--- __tests__/tactics.js
+++ __tests__/tactics.js
@@ -19,4 +19,29 @@ describe('Tactics', function() {
       expect(result.length).toEqual(0);
     });
   });
+
+  describe(',getCommonCartItems', function() {
+    it('should return cartItem_length_is_zero', function() {
+      var Tactics = require('../src/model/tactics.js');
+      getName = jest.genMockFn();
+      getName.mockReturnValue('可口可乐350ml');
+
+      var cartItems = [{'item' :
+                       {'barcode' :'ITEM000000',
+                        'name' : '可口可乐350ml',
+                        'unit' : '瓶',
+                        'price' : 3.00,
+                        'brand' : '可口可乐'},
+                       'count' : 20,
+                       'promotion' : true,
+                       getName : getName}];
+      var itemName = '雪碧';
+      var result = Tactics.getCommonCartItems(cartItems, itemName);
+
+      expect(result[0].item.name).toEqual('可口可乐350ml');
+      expect(result[0].item.barcode).toEqual('ITEM000000');
+      expect(result[0].item.price).toEqual(3);
+
+    });
+  });
 });
