a0a4f4976c5fdf0fc3c595277f3012b8f0f5ec20
liurong <570899307@qq.com>
2015-02-13 21:44:42 -0500

    use node test main.js_is_pass

---
+++ src/main.js
@@ -0,0 +1,21 @@
+var Scanner = require('./model/scanner.js');
+var Invoice = require('./model/invoice.js');
+
+function main() {
+  var tag = [{ 'ITEM000000' : 20 },
+             { 'ITEM000010' : 30 },
+             { 'ITEM000005' : 30 },
+             { 'ITEM000008' : 25 },
+             { 'ITEM000003' : 8  },
+             { 'ITEM000002' : 14 }];
+
+  var scanner = new Scanner();
+  var cartItems = scanner.addCartItems(tag);
+  var invoice = new Invoice();
+  var tacticsType = 3;
+  var print = invoice.printInventory(cartItems, tacticsType);
+
+  console.log(print);
+}
+
+main();
