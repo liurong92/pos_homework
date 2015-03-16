52ea5f14593004e2c53b2d74d1c89788d5ad54cb
liurong <570899307@qq.com>
2015-03-15 20:44:04 -0400

    test scanner

---
+++ pos_v3.1/__tests__/scanner_tests.js
@@ -0,0 +1,55 @@
+jest.autoMockOff();
+jest.dontMock('lodash');
+
+describe('Scanner', function () {
+  var Scanner, scanner;
+  beforeEach(function () {
+    Scanner = require('../src/model/scanner.js');
+    scanner = new Scanner();
+  });
+
+  describe('#addCartItems', function () {
+    it('should return one cartItem is brand', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 }]);
+      expect(result[0].item.brand).toEqual('可口可乐');
+    });
+
+    it('should return two cartItems', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+      expect(result.length).toBe(2);
+    });
+
+    it('should return two cartItems_brand', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+
+      expect(result[0].item.brand).toEqual('可口可乐');
+      expect(result[1].item.brand).toEqual('可口可乐');
+    });
+
+    it('should return two cartItems_barcode', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+
+      expect(result[0].item.barcode).toEqual('ITEM000000');
+      expect(result[1].item.barcode).toEqual('ITEM000010');
+    });
+
+    it('should return two cartItems_name', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+
+      expect(result[0].item.name).toEqual('可口可乐350ml');
+      expect(result[1].item.name).toEqual('可口可乐550ml');
+    });
+
+    it('should return two cartItems_price', function () {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+
+      expect(result[0].item.price).toEqual(3);
+      expect(result[1].item.price).toEqual(4);
+    });
+  });
+});
---
+++ pos_v3.1/src/model/scanner.js
@@ -0,0 +1,21 @@
+var _ = require('lodash');
+var Item = require('./item.js');
+var CartItem = require('./cartitem.js');
+
+function Scanner () {
+
+}
+
+Scanner.prototype.addCartItems = function (tag) {
+  var cartItems = [];
+  for(var i = 0; i < tag.length; i++) {
+    for(var key in tag[i]) {
+      var item = _.find(Item.all(), {'barcode': key});
+      cartItems.push(new CartItem(item, tag[i][key]));
+    }
+  }
+
+  return cartItems;
+};
+
+module.exports = Scanner;
